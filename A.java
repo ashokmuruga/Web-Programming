a635d3325e01a1e95855b69fbf729837e6c683e9
Ashok M <haitwoashok@gmail.com>
2020-08-07 17:45:23 +0530

    Packages

---
+++ A.java
@@ -0,0 +1,4 @@
+package pack;  
+public class A{  
+  public void msg(){System.out.println("Hello");}  
+}  
---
+++ B.java
@@ -0,0 +1,8 @@
+import pack.*;  
+  
+class B{  
+  public static void main(String args[]){  
+   A obj = new A();  
+   obj.msg();  
+  }  
+}  
